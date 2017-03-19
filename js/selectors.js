a34990cfcff23932ac05c1a0ec5ed5fcd17a7b80
John Shelby <johnshelby@yahoo.com>
2017-03-19 17:34:57 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+  pTags = $('p')
+  return pTags
+}
+
+function lastImageSelector() {
+  lastImage = $('img:last')
+  return lastImage
+}
+
+function ninjaBabySelector() {
+  nBS = $('#baby-ninja')
+  return nBS
+}
+
+function divSelector() {
+  picDivs = $('.pics')
+  return picDivs
+}
+
+function firstListItem() {
+  firstList = $('#pic-list li:first-child')
+  return firstList
+ }
